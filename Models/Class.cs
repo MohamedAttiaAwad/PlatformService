21e494d0bbdda8ef0b3c77a02f475e79fae477cf
mohamed attia <devm3aatya@gmail.com>
2022-01-23 23:05:36 +0300

    platformservice

---
+++ Models/Class.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Models
+{
+    public class Class
+    {
+        public void test([FromServices] IPlatformRepo repo)
+        {
+
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -55,6 +55,8 @@ namespace PlatformService
             {
                 endpoints.MapControllers();
             });
+
+            app.Run
         }
     }
 }
